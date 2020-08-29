0f8ca296d6862eda06072d78abab78ae25125450
leonhelg <20755938+leonhelg@users.noreply.github.com>
2020-08-29 05:41:27 +0200

    v 0.0.1 Complete

--- calc-price-arbgivavgift.py
+++ calc-price-arbgivavgift.py
@@ -2,7 +2,7 @@
 
 """This application calculates useful numbers for my companies"""
 
-def input_price(prompt = 'Please write the price you want converted:'):
+def input_price(prompt = 'Please write the price you want converted: '):
     return input(prompt)
 
 def arbgivavgift(amount):
@@ -11,21 +11,31 @@ def arbgivavgift(amount):
 def tax(amount):
     return amount*0.25,amount*1.25
 
-def view():
-    input_p = input_price()
+def bad_factory():
+    input_p = float(input_price())
     arbgivavg, arbgivavgift_total = arbgivavgift(input_p)
     tax_base,tax_total = tax(arbgivavgift_total)
-    print(f'Ferdig pris i kr inkl.mva: {tax_total} \n\n Skatt 25%: {tax_base}')
-    print(f'Arbeidsgiveravgift 14,1%: {arbgivavg} \n Forrige pris: {input_p}')
+    return input_p,tax_total,arbgivavg,tax_base
+
+def view():
+    input_p,tax_total,arbgivavg,tax_base = bad_factory()
+    print(f'\nFerdig pris i kr inkl.mva: {tax_total:.2f}\n')
+    print(f' Ferdig pris i kr eks.mva: {tax_total-tax_base:.2f}')
+    print(f'                Skatt 25%: {tax_base:.2f}')
+    print(f' Arbeidsgiveravgift 14,1%: {arbgivavg:.2f}')
+    print(f'             Forrige pris: {input_p:.2f}')
 
 def isRunning():
     isRunning = True
+    
     while isRunning == True:
         usr_input = input('\n\nThis application makes a new price for Aksjeselskap\n\nMain Menu\nPress enter to run the program or q to quit\n')
         if usr_input.lower != 'q':
             view()
-            usr_input = input('The program has ended, press ENTER to run again or "q" to quit')
+            usr_input = input('The program has ended, press ENTER to run again or "q" to quit \n')
             if usr_input.lower == 'q':
                 isRunning = False
+        else:
+            isRunning = False
 
 isRunning()
